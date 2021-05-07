dfab0ecd604d70c3269814d13c5106cd89d6522f
Ing Miguel Martinez <mai.djmai@gmail.com>
2021-05-06 22:18:00 -0500

    no message

--- volumes/logstash/config/queries/product-quantity.sql
+++ volumes/logstash/config/queries/product-quantity.sql
@@ -1,1 +1,34 @@
-SELECT * FROM adventureworks.vw_productos
+SELECT
+	`p`.`ProductID` AS `ProductID`,
+	`p`.`Name` AS `Name`,
+	`p`.`ProductNumber` AS `ProductNumber`,
+	`p`.`MakeFlag` AS `MakeFlag`,
+	`p`.`FinishedGoodsFlag` AS `FinishedGoodsFlag`,
+	`p`.`Color` AS `Color`,
+	`p`.`SafetyStockLevel` AS `SafetyStockLevel`,
+	`p`.`ReorderPoint` AS `ReorderPoint`,
+	`p`.`StandardCost` AS `StandardCost`,
+	`p`.`ListPrice` AS `ListPrice`,
+	`p`.`Size` AS `Size`,
+	`p`.`SizeUnitMeasureCode` AS `SizeUnitMeasureCode`,
+	`p`.`WeightUnitMeasureCode` AS `WeightUnitMeasureCode`,
+	`p`.`Weight` AS `Weight`,
+	`p`.`DaysToManufacture` AS `DaysToManufacture`,
+	`p`.`ProductLine` AS `ProductLine`,
+	`p`.`Class` AS `Class`,
+	`p`.`Style` AS `Style`,
+	`p`.`ProductSubcategoryID` AS `ProductSubcategoryID`,
+	`p`.`ProductModelID` AS `ProductModelID`,
+	`p`.`SellStartDate` AS `SellStartDate`,
+	`p`.`SellEndDate` AS `SellEndDate`,
+	`p`.`DiscontinuedDate` AS `DiscontinuedDate`,
+	`p`.`ModifiedDate` AS `ModifiedDate`,
+	count( `i`.`Quantity` ) AS `Quantity` 
+FROM
+	(
+		`adventure.product` `p`
+		LEFT JOIN `adventure.productinventory` `i` ON ((
+				`p`.`ProductID` = `i`.`ProductID` 
+			))) 
+GROUP BY
+	`p`.`ProductID`
