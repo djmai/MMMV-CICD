bd8cb0942bb209a899978f3553a43042c7f12ccd
Ing Miguel Martinez <mai.djmai@gmail.com>
2021-05-06 21:56:46 -0500

    no message

--- update.sh
+++ update.sh
@@ -5,23 +5,12 @@ echo '===   MTIE MIGUEL MANUEL MARTINEZ VAZQUEZ              ==='
 echo '=========================================================='
 
 echo '=========================================================='
-echo '=== PASO 1: CONFIGURACION DE VARAIBLE VM.MAX_MAP_COUNT ==='
+echo '===        PASO 0: LIMPIADNO CONTENEDORES              ==='
 echo '=========================================================='
-sudo sysctl -w vm.max_map_count=262144
-sudo sysctl -p
+docker rm -f $(docker ps -qa)
 
 echo '=========================================================='
-echo '===       PASO 2: INSTALACION DE DOCKER-COMPOSE        ==='
-echo '=========================================================='
-sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-echo '=========================================================='
-echo '===           PASO 3: FIN DEL SCRIPT CONFIG            ==='
-echo '=========================================================='
-
-echo '=========================================================='
-echo '===          PASO 4: LIMPIADNO REPO LOCAL              ==='
+echo '===          PASO 1: LIMPIADNO REPO LOCAL              ==='
 echo '=========================================================='
 if [ -d ~/MMMV-CICD/ ]; then
     echo 'sudo rm -R MMMV-CICD'
@@ -31,18 +20,18 @@ else
 fi
 
 echo '=========================================================='
-echo '===           PASO 5: CONFIGURANCDO GIT                ==='
+echo '===           PASO 2: INSTALANDO GIT                   ==='
 echo '=========================================================='
 alias git="docker run -ti --rm -v $(pwd):/git bwits/docker-git-alpine"
 
 echo '=========================================================='
-echo '===           PASO 6: CLONANDO REPO                    ==='
+echo '===           PASO 3: CLONANDO REPO                    ==='
 echo '=========================================================='
 git clone https://github.com/djmai/MMMV-CICD.git
 cd MMMV-CICD
 
 echo '=========================================================='
-echo '===           PASO 8: COPIANDO DATA                    ==='
+echo '===           PASO 4: COPIANDO DATA                    ==='
 echo '=========================================================='
 if [ -d ./volumes/ ]; then
     sudo cp -U volumes/ ~/
@@ -62,7 +51,7 @@ else
 fi
 
 echo '=========================================================='
-echo '===          PASO 7: DESPLEGANDO CONTENEDORES          ==='
+echo '===          PASO 5: DESPLEGANDO CONTENEDORES          ==='
 echo '=========================================================='
 echo 'sudo docker-compose up --build -d'
 sudo docker-compose up --build -d
